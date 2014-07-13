5b333b83a834580302219a6d935668efd21fff94
Eric Internicola <intere@gmail.com>
2014-07-13 10:34:58 -0600

    master: Default (basic config) for my web server.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'appserver'
+maintainer       'colasoft'
+maintainer_email 'intere@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures appserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends		 'apache2'
---
+++ recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: appserver
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "apache2"
+include_recipe "apache2::mod_ssl"
+
+package "mod_ssl" do
+  action :install
+end
+
+apache_site "default" do
+  enable false
+end
+
+execute "change permission web" do
+  command "chmod -R 0777 /vagrant/web"
+  action :run
+end
+
+web_app 'insecure' do
+  server_name node['hostname']
+  template 'apache_insecure_conf.erb'
+end
+
+execute "/usr/bin/openssl genrsa -out server.key 2048" do
+  cwd '/etc/httpd/ssl'
+  not_if { ::File.exists?('/etc/httpd/ssl/server.key') }
+end
+
+execute "/usr/bin/openssl req -new -x509 -key server.key -out server.crt -days 1095 -batch" do
+  cwd '/etc/httpd/ssl'
+  not_if { ::File.exists?('/etc/httpd/ssl/server.crt') }
+end
+
+web_app 'secure' do
+  server_name node['hostname']
+  template 'apache_secure_conf.erb'
+end
+
