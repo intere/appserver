2ed7d1ff063fd23b22b480484ebe77ad35328fa4
Eric Internicola <einternicola@silverchalice.com>
2014-07-15 17:10:42 -0600

    develop: Added some comments.

--- recipes/mongo.rb
+++ recipes/mongo.rb
@@ -4,6 +4,9 @@
 
 include_recipe "mongodb::default"
 
+#
+# Import data if it exists
+#
 if File.exists?('/vagrant/apps/mongo/dump')
   execute "Import Mongo Data" do
     command "mongorestore /vagrant/apps/mongo/dump"
